69af58b830e8cd233c2145f1ccf3268e421a820d
Emerson Carvalho <emerson@MacBook-Pro-de-Emerson.local>
2022-04-19 12:07:22 -0300

    Versão última aula

--- app/src/main/java/br/edu/ifsuldeminas/login/MainActivity.java
+++ app/src/main/java/br/edu/ifsuldeminas/login/MainActivity.java
@@ -1,15 +1,21 @@
 package br.edu.ifsuldeminas.login;
 
+import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
+import android.graphics.Bitmap;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.widget.EditText;
+import android.widget.ImageView;
 import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
 
-    public static final String TAG = "login_main_activity";
+    private static final String TAG = "login_main_activity";
+    private static final int REQUEST_FOTO_CODE = 1;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -18,14 +24,17 @@ public class MainActivity extends AppCompatActivity {
 
         Button buttonLogin = (Button) findViewById(R.id.buttonLogin);
         buttonLogin.setOnClickListener(new View.OnClickListener() {
-
             @Override
             public void onClick(View view) {
-                Toast toast = Toast.makeText(getApplicationContext(),
-                        "Clicou no botão Logar",
-                        Toast.LENGTH_LONG);
+                Intent welcomeIntent = new Intent(getApplicationContext(),
+                        WelcomeActivity.class);
 
-                toast.show();
+                EditText userName = (EditText) findViewById(R.id.editTextUser);
+                String name = userName.getText().toString();
+
+                welcomeIntent.putExtra("USER", name);
+
+                startActivity(welcomeIntent);
             }
         });
 
@@ -34,7 +43,7 @@ public class MainActivity extends AppCompatActivity {
     }
 
     public void signIn(View view){
-        Toast toast = Toast.makeText(getApplicationContext(),
+        Toast toast = Toast.makeText(view.getContext(),
                 "Clicou no botão Cadastrar",
                 Toast.LENGTH_LONG);
 
---
+++ app/src/main/java/br/edu/ifsuldeminas/login/WelcomeActivity.java
@@ -0,0 +1,90 @@
+package br.edu.ifsuldeminas.login;
+
+import androidx.annotation.Nullable;
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.graphics.Bitmap;
+import android.os.Bundle;
+import android.provider.MediaStore;
+import android.util.Log;
+import android.view.View;
+import android.widget.ImageView;
+import android.widget.Toast;
+
+import com.google.android.material.snackbar.Snackbar;
+
+public class WelcomeActivity extends AppCompatActivity {
+
+    private final String TAG = "login-bem-vindo";
+    private static final int REQUEST_PICTURE_CODE = 1;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_welcome);
+
+        Intent callingIntent =  getIntent();
+
+        String name = callingIntent.getStringExtra("USER");
+        String welcome = String.format("Bem vindo '%s'!", name);
+
+        View layout = findViewById(R.id.activity_bem_vindo_id);
+        Snackbar snackbar = Snackbar.make(layout, welcome, Snackbar.LENGTH_LONG);
+        snackbar.show();
+    }
+
+    @Override
+    protected void onStart() {
+        super.onStart();
+        Log.i(TAG, "Rodou o método onStart()");
+    }
+
+    @Override
+    protected void onResume() {
+        super.onResume();
+        Log.i(TAG, "Rodou o método onResume()");
+    }
+
+    @Override
+    protected void onPause() {
+        super.onPause();
+        Log.i(TAG, "Rodou o método onPause()");
+    }
+
+    @Override
+    protected void onStop() {
+        super.onStop();
+        Log.i(TAG, "Rodou o método onStop()");
+    }
+
+    @Override
+    protected void onRestart() {
+        super.onRestart();
+        Log.i(TAG, "Rodou o método onRestart()");
+    }
+
+    @Override
+    protected void onDestroy() {
+        super.onDestroy();
+        Log.i(TAG, "Rodou o método onDestroy()");
+    }
+
+    public void takePictureClick(View view){
+        Intent takePictureIntent = new Intent(MediaStore.ACTION_IMAGE_CAPTURE);
+        startActivityForResult(takePictureIntent, REQUEST_PICTURE_CODE);
+    }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, @Nullable Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+
+        if(requestCode == REQUEST_PICTURE_CODE && resultCode == RESULT_OK && data != null){
+            Bundle extras = data.getExtras();
+            Bitmap picture = (Bitmap) extras.get("data");
+
+            ImageView imageView = findViewById(R.id.imagePictureViewId);
+            imageView.setImageBitmap(picture);
+        }
+    }
+}
